35035d3cdd75333406ad793526991b5d0cd19df1
Tanjid Hasan Tonmoy <tanjidhasantonmoy21@gmail.com>
2025-03-06 16:00:39 +0100

    First Commit

---
+++ src/main/java/com/tanjidd/TanjidHotel/TanjidHotelApplication.java
@@ -0,0 +1,13 @@
+package com.tanjidd.TanjidHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanjidHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanjidHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanjidd/TanjidHotel/TanjidHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanjidd.TanjidHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanjidHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
