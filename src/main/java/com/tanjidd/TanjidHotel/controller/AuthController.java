2434f6f3220e449b50b04662dd4aa4b82fccbdc4
Tanjid Hasan Tonmoy <tanjidhasantonmoy21@gmail.com>
2025-03-11 13:07:29 +0100

    Added controllers

---
+++ src/main/java/com/tanjidd/TanjidHotel/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.tanjidd.TanjidHotel.controller;
+
+
+import com.tanjidd.TanjidHotel.dto.LoginRequest;
+import com.tanjidd.TanjidHotel.dto.Response;
+import com.tanjidd.TanjidHotel.entity.User;
+import com.tanjidd.TanjidHotel.service.interfac.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
